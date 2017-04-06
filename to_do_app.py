62ae074c7b0163d93cd08db37801aae0add4c30d
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-04-06 15:27:37 +0200

    List out with -l

--- to_do_app.py
+++ to_do_app.py
@@ -1,30 +1,55 @@
 import sys
 
-#myfile = open('duplicated-chars.txt', 'r')
-
 class Controller():
-	#def __init__(self, View):
 
+	def __init__(self):
+		self.list_argv = []
+		self.arg_reader()
+		self.control()
 
 	def arg_reader(self):
-		self.list_argv = []
 		if len(sys.argv) <= 1:
 			self.list_argv = []
 		else:
 			self.list_argv = sys.argv[1:]
 
+
+	def control(self):
 		if len(self.list_argv) == 0:
 			view.print_welcome_page()
 		else:
-			pass
-			# if( arguments[0] == '-l' ):
-			# 	print('Addolunk ilyet', arguments[1])
+			if self.list_argv[0] == '-l':
+				self.print_list()
+
+	def print_list(self):
+		if len(model.to_do) == 0:
+		    print('Nothing to do today')
+		else:
+			for i in range(len(model.to_do)):
+				print(i + 1, model.to_do[i][:-1])
+
+			# if( self.list_argv[0] == '-a' ):
+			# if( self.list_argv[0] == '-r' ):
+			# if( self.list_argv[0] == '-c' ):
 
 #arguments = arg_reader()
 
 
+class Model():
 
-#class Model(Controller):
+	def __init__(self):
+		self.to_do = ""
+		self.reader()
+
+
+	def reader(self):
+		try:
+			myfile = open('database.txt', 'r')
+			self.to_do = myfile.readlines()
+			myfile.close()
+		except:
+			self.to_do = []
+
 
 class View():
 
@@ -37,6 +62,9 @@ class View():
 
 
 view = View()
+model = Model()
 controller = Controller()
+
+
 
 controller.arg_reader()
